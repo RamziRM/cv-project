5aeddb44a500c77fcefd5de68b503cfa9a6c23b4
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-04-17 23:55:27 -0300

    transform to functional comp

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,148 +1,133 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "../styles/PersonalInfo.css";
 
-class PersonalInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            personalInfo: {
-                firstName: "",
-                lastName: "",
-                email: "",
-                phone: "",
-                location: "",
-                github: "",
-                website: ""
-            },
-            editMode: true,
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    }
+const PersonalInfo = () => {
+    const [personalInfo, setPersonalInfo] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        location: "",
+        github: "",
+        website: ""
+    });
+
+    const [editMode, setEditMode] = useState(true);
 
     // handleSubmit should toggle editMode to !prevState.editMode
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.setState({
-            editMode: !this.state.editMode
-        });
+        setEditMode(editMode);
     }
 
     // handleChange should update the state with the new value
-    handleChange = (e) => {
+    const handleChange = (e) => {
         const { name, value } = e.target;
-        this.setState((prevState) => ({
-                personalInfo: {
-                    ...prevState.personalInfo,
-                    [name]: value
-                }
-            })
-        );
+        setPersonalInfo((prevState) => ({
+                ...prevState.personalInfo,
+                [name]: value
+            }));
     }
-
-    render() {
-        const { editMode, personalInfo } = this.state;
-        // order of destructuring matters ***
-        const { firstName, lastName, email, phone, location, github, website } = personalInfo;
-        // if editMode is false, render the personal info
-        if (!editMode) {
-            return (
-                <div className="read-cont">
-                    <h2>Personal Information</h2>
-                    <p><strong>First name:</strong> {personalInfo.firstName}</p>
-                    <p>Last name: {personalInfo.lastName}</p>
-                    <p>Email: {personalInfo.email}</p>
-                    <p>Phone: {personalInfo.phone}</p>
-                    <p>Location: {personalInfo.location}</p>
-                    <p>GitHub: {personalInfo.github}</p>
-                    <p>Website: {personalInfo.website}</p>
-                    <button 
-                    type="button"
-                    className="btn"
-                    onClick={this.handleSubmit}
-                    >
-                        Edit
-                    </button>
-                </div>
-            );
-        }
-
+    
+    const { firstName, lastName, email, phone, location, github, website } = personalInfo;
+    // if editMode is false, render the personal info
+    if (!editMode) {
         return (
-        <div>
-            <h2>Personal Information</h2>
-            <form className="personalForm" onSubmit={this.handleSubmit}>
-                <label htmlFor="firstName">First name</label>
-                <input 
-                type="text" 
-                id="firstName" 
-                name="firstName"
-                placeholder="First name"
-                required
-                onChange={this.handleChange}
-                value={firstName}
-                 />
-                <label htmlFor="lastName">Last name</label>
-                <input 
-                type="text" 
-                id="lastName" 
-                name="lastName"
-                placeholder="Last name"
-                required
-                onChange={this.handleChange}
-                value={lastName}
-                 />
-                <label htmlFor="email">Email</label>
-                <input
-                 type="email"
-                 id="email" 
-                 name="email"
-                 required
-                 onChange={this.handleChange}
-                 value={email}
-                 />
-                <label htmlFor="phone">Phone</label>
-                <input 
-                type="text" 
-                id="phone" 
-                name="phone" 
-                onChange={this.handleChange}
-                value={phone}
-                />
-                <label htmlFor="location">Location</label>
-                <input 
-                type="text" 
-                id="location" 
-                name="location"
-                onChange={this.handleChange}
-                value={location}
-                />
-
-                <label htmlFor="github">GitHub</label>
-                <input 
-                type="text" 
-                id="github" 
-                name="github"
-                onChange={this.handleChange}
-                value={github}
-                />
-                <label htmlFor="website">Website</label>
-                <input 
-                type="text" 
-                id="website" 
-                name="website" 
-                onChange={this.handleChange}
-                value={website}
-                />
+            <div className="read-cont">
+                <h2>Personal Information</h2>
+                <p><strong>First name:</strong> {personalInfo.firstName}</p>
+                <p>Last name: {personalInfo.lastName}</p>
+                <p>Email: {personalInfo.email}</p>
+                <p>Phone: {personalInfo.phone}</p>
+                <p>Location: {personalInfo.location}</p>
+                <p>GitHub: {personalInfo.github}</p>
+                <p>Website: {personalInfo.website}</p>
                 <button 
-                type="submit"
+                type="button"
                 className="btn"
+                onClick={handleSubmit}
                 >
-                    Save
+                    Edit
                 </button>
-            </form>
-        </div>
+            </div>
         );
     }
+
+    return (
+    <div>
+        <h2>Personal Information</h2>
+        <form className="personalForm" onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First name</label>
+            <input 
+            type="text" 
+            id="firstName" 
+            name="firstName"
+            placeholder="First name"
+            required
+            onChange={handleChange}
+            value={firstName}
+                />
+            <label htmlFor="lastName">Last name</label>
+            <input 
+            type="text" 
+            id="lastName" 
+            name="lastName"
+            placeholder="Last name"
+            required
+            onChange={handleChange}
+            value={lastName}
+                />
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                id="email" 
+                name="email"
+                required
+                onChange={handleChange}
+                value={email}
+                />
+            <label htmlFor="phone">Phone</label>
+            <input 
+            type="text" 
+            id="phone" 
+            name="phone" 
+            onChange={handleChange}
+            value={phone}
+            />
+            <label htmlFor="location">Location</label>
+            <input 
+            type="text" 
+            id="location" 
+            name="location"
+            onChange={handleChange}
+            value={location}
+            />
+
+            <label htmlFor="github">GitHub</label>
+            <input 
+            type="text" 
+            id="github" 
+            name="github"
+            onChange={handleChange}
+            value={github}
+            />
+            <label htmlFor="website">Website</label>
+            <input 
+            type="text" 
+            id="website" 
+            name="website" 
+            onChange={handleChange}
+            value={website}
+            />
+            <button 
+            type="submit"
+            className="btn"
+            >
+                Save
+            </button>
+        </form>
+    </div>
+    );
 }
 
 export default PersonalInfo;
